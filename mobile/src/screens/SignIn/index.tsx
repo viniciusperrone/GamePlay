4aed3e22a63a4e517ed1e8d07981c5002cf5356e
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-06-20 20:58:49 -0400

    SignIn full screen

--- mobile/src/screens/SignIn/index.tsx
+++ mobile/src/screens/SignIn/index.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StatusBar,
+    Button
+} from 'react-native';
 
-import {} from 'react-native';
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './style';
 
 const SignIn: React.FC = () => {
     return (
-        <>
-        </>
+        <View style={styles.container}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+
+            <Image
+                source={IllustrationImg}
+                style={styles.image}
+                resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <Button
+                    title="Entrar com Discord"
+                    activeOpacity={0.7}
+                />
+
+            </View>
+        </View>
     )
 }
 
-export default SignIn; 
+export default SignIn;
